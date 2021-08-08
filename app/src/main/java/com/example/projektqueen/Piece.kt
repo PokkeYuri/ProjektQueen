43e5e2a83f02785297efb4355d25b6c6f184130c
PokkeYuri <86960788+PokkeYuri@users.noreply.github.com>
2021-08-08 21:46:34 +0200

    Piece.kt hinzugefügt

--- app/src/main/java/com/example/projektqueen/Piece.kt
+++ app/src/main/java/com/example/projektqueen/Piece.kt
@@ -1,4 +1,20 @@
 package com.example.projektqueen
 
-class Piece {
+class Pawn(posX: Int, posY: Int, color: String) {
 }
+
+class Rook(posX: Int, posY: Int, color: String) {
+}
+
+class Knight(posX: Int, posY: Int, color: String) {
+}
+
+class Bishop(posX: Int, posY: Int, color: String) {
+}
+
+class Queen(posX: Int, posY: Int, color: String) {
+}
+
+class King(posX: Int, posY: Int, color: String) {
+}
+
