2440b27ec0a9d588850f6c620cbf02431ee2de9c
PokkeYuri <86960788+PokkeYuri@users.noreply.github.com>
2021-08-08 18:38:12 +0200

    Initial commit

---
+++ app/src/main/java/com/example/projektqueen/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.projektqueen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
