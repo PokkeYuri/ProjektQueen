c024f8c00f7623d59e887df9c0a5b304a11f8d0d
smokR47 <83354742+smokR47@users.noreply.github.com>
2021-08-09 02:21:16 +0200

    changed color : String to color : Char

--- app/src/main/java/com/example/projektqueen/Piece.kt
+++ app/src/main/java/com/example/projektqueen/Piece.kt
@@ -1,20 +1,20 @@
 package com.example.projektqueen
+class Piece {
+    class Pawn(posX: Int, posY: Int, color: Char) {
+    }
 
-class Pawn(posX: Int, posY: Int, color: String) {
+    class Rook(posX: Int, posY: Int, color: Char) {
+    }
+
+    class Knight(posX: Int, posY: Int, color: Char) {
+    }
+
+    class Bishop(posX: Int, posY: Int, color: Char) {
+    }
+
+    class Queen(posX: Int, posY: Int, color: Char) {
+    }
+
+    class King(posX: Int, posY: Int, color: Char) {
+    }
 }
-
-class Rook(posX: Int, posY: Int, color: String) {
-}
-
-class Knight(posX: Int, posY: Int, color: String) {
-}
-
-class Bishop(posX: Int, posY: Int, color: String) {
-}
-
-class Queen(posX: Int, posY: Int, color: String) {
-}
-
-class King(posX: Int, posY: Int, color: String) {
-}
-
